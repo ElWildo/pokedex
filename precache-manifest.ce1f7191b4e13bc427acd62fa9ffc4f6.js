6ef62179540bf07d35943544129cf512a17f0d5c
Francesco Wild <francescowild90@gmail.com>
2019-08-18 18:02:22 +0100

    Updates

---
+++ precache-manifest.ce1f7191b4e13bc427acd62fa9ffc4f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06ad72a0bc03b3ebdbbed1353b6ac7e",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "6fcc32ba8725a866af1b",
+    "url": "/pokedex/static/css/main.580848e1.chunk.css"
+  },
+  {
+    "revision": "34bb11d5ffa2278f593d",
+    "url": "/pokedex/static/js/2.a999a849.chunk.js"
+  },
+  {
+    "revision": "6fcc32ba8725a866af1b",
+    "url": "/pokedex/static/js/main.551026aa.chunk.js"
+  },
+  {
+    "revision": "e2590d326f0902d7440b",
+    "url": "/pokedex/static/js/runtime~main.9b58ce06.js"
+  }
+]);
